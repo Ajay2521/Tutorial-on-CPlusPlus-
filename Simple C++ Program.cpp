e5457994ee315b70b1b10cbc08d84bee918dfc97
MAAYON_AJ <MAAYON@MAAYON_AJ>
2020-06-03 11:18:37 +0100

    Simple C++ program which is explained how C++ program looks and works

---
+++ Simple C++ Program.cpp
@@ -0,0 +1,26 @@
+//Note any statement which is present inside a double black slash then it is called as Comments which will be ignored by C++ complier while compling the C++ program source code
+
+//The first line of the program consist of preprocessor commands or header files which is used to inculde a pre-defined functions avaliable in c++ progarmming which is needed to built the c++ program
+
+#include <iostream>
+ 
+//Then in next line we use namespace , which tell the complier to use namespace
+
+using namespace std ;
+
+//Then we create the fuction where statements and codes will be present and which will be working when the program is compiled
+
+//Note: A C++ program must contain atleast one main () function in a entier C++ program
+  
+int main()
+{
+
+// cout is standard per defined function which is used to print the statement which is present inside the double quotes and terminated by a colon " ; " 
+
+	cout<<"Hello, Its is an Simple C++ program.!" ;
+
+/*return will be used is return the value of a function , it was an optional one */
+
+return 0;
+
+}
