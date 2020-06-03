64074c5ec747093ef9253a4f8f32ee3b9630e458
MAAYON_AJ <MAAYON@MAAYON_AJ>
2020-06-03 21:47:43 +0100

    In this Escape Sequence or Special Characters in C++ program has been explained

---
+++ Escape Sequence.cpp
@@ -0,0 +1,43 @@
+/*Escape sequence or special characters which is used for specific task in a C++ program*/
+
+/*inculding preprocessor*/
+
+#include <iostream>
+
+/*using namespace*/
+
+using namespace std ;
+
+/*main function of a programing*/
+
+int main()
+
+{
+
+/*Here Manjor Escape Sequence are explained here*/
+
+cout<<"\nBalck Slash will be printed : \\ "<<endl ;
+
+cout<<"\nSingle quotes will be printed : \' "<<endl ;
+
+cout<<"\nDouble quotes will be printed : \'' "<<endl ;
+
+cout<<"\nQuestion mark will be printed : \? "<<endl ;
+
+cout<<"\nAlert or bell sound will be produced :  \a "<<endl ;
+
+cout<<"\nBackSpace Key function will be done : \b\bsuccessfully "<<endl ;
+
+cout<<"\nForm Feed will be printed : \f successfully in the program  "<<endl ;
+
+cout<<"\nNew line will be printed : \nsuccessfully "<<endl ;
+
+cout<<"\nHorizontal tab will be printed : \tsuccessfully "<<endl ;
+
+cout<<"\nVertical tab will be printed : \vsuccessfully "<<endl ;
+
+}
+
+
+
+ 
