c177edc8f163e95b1509de06a55f7e67f632e072
MAAYON_AJ <MAAYON@MAAYON_AJ>
2020-06-03 13:04:16 +0100

    In this Variable in C++ program has been explained

---
+++ Variable.cpp
@@ -0,0 +1,34 @@
+/*Variable in C++ program is used to define a Variable to store specific value at specific storage memory*/ 
+
+/*inculding preprocessor*/
+
+#include<iostream>
+
+/*using namespace*/
+
+using namespace std ;
+
+/*main function of a programing*/
+
+int main()
+{
+
+/*Note: the syntax for varibale definition is "data_type variable_name = value; " or "data_type variable_name; " or "data_type variable_name1,variable_name2,,.....variable_namen;"*/
+
+/* A Variable Name can be of any user defined here in this program i have given a,b,c as a variable name*/
+ 
+int a= 5;
+
+char  b='a';
+
+float c=20.450;
+
+/*Accessing the variable value by using the variable name*/
+
+cout<<"\nThe value of int a is : "<<a<<endl ;
+
+cout<<"\nThe value of char b is : "<<b<<endl ;
+
+cout<<"\nThe value of float c is : "<<c<<endl ;
+
+}
