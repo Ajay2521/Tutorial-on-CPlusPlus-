f6ddee9f74834368980ec764681a7336c6cfaf81
MAAYON_AJ <MAAYON@MAAYON_AJ>
2020-06-04 09:27:43 +0100

    In this Operator Precedence concept in C++ program has been explained

---
+++ Operator Precedence.cpp
@@ -0,0 +1,67 @@
+/*In this program we will be Operators Precedence in C++ it as same as BEDMAS which we use in Math*/
+
+/*Operators Precedence is used to determines how to group and execute the expressions in c++ program*/
+
+/*In C++ all postfix operation like braces,square bracket, increment , decrement have highgher precedence */
+
+/*Then comes the Unary opertors followed by Division,Multiplication,Addition , Subtaraction , Then Shift Operation , Then comes Relatipnal Opeartion, Then Equalities ,All Bitwise Operation,Then comes All logical Operation,Then comes All Assignment  Operation*/
+
+/*including preprocessor / header file in the program*/
+
+#include <iostream>
+
+/*using namespace*/
+
+using namespace std ;
+
+/*creating a main() function of the program*/
+
+int main()
+
+{
+
+/*Declearing Variable 'a=20' ,Variable 'b=10' ,Variable 'c=15' , Variable 'd=25'and creating a Variable 'e' with no initial value declear*/ 
+
+int a = 20;
+
+int b = 10;
+
+int c = 15;
+
+int d = 25;
+
+int e;
+
+e = a + b * c / d ; /* 20 + 10 * 15 / 25 */ 
+
+cout<<"\nValue of  a + b * c / d is e = " << e << endl ;
+
+e = ( a + b ) * ( c / d ) ; /* ( 20 + 10 ) * ( 15 / 25 ) */ 
+
+cout<<"\nValue of ( a + b ) * ( c / d ) is e = " << e << endl ;
+
+e = ( a + b ) *  c / d  ; /* ( 20 + 10 ) * 15 / 25  */ 
+
+cout<<"\nValue of ( a + b ) *  c / d  is e = " << e << endl ;
+
+e = ( ( a + b ) *  c ) / d  ; /* ( ( 20 + 10 ) * 15 ) / 25  */ 
+
+cout<<"\nValue of ( ( a + b ) *  c ) / d  is e = " << e << endl ;
+
+e =  a + ( b  *  c / d ) ;  /* 20 + ( 10  * 15 / 25 ) */ 
+
+cout<<"\nValue of  a +(  b *  c / d ) is e = " << e << endl ;
+
+e =  a + ( b  *  c ) / d  ; /*  20 + ( 10  *  15 ) / 25  */ 
+
+cout<<"\nValue of  a + ( b  *  c ) / d   is e = " << e << endl ;
+
+e = ( a + ( b  *  c ) ) / d  ; /* ( 20 + ( 10  *  15 ) ) / 25  */ 
+
+cout<<"\nValue of  ( a + ( b  *  c ) ) / d   is e = " << e << endl ;
+
+}
+
+
+
+
