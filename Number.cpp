a1d580d4fbec73e33a959b1a27ecbe25f5b8143a
MAAYON_AJ <MAAYON@MAAYON_AJ>
2020-06-05 11:33:14 +0100

    In this Number concept in C++ program has been explained

---
+++ Number.cpp
@@ -0,0 +1,43 @@
+/*In this lets see more about NUMBERS in c++ program.*/
+
+/*In c++ program we usually use int, float, short, long are major Data Types used for defining and using numbers.*/
+
+/*including preprocessor in the program*/
+
+#include <iostream>
+
+/*using namespace*/
+
+using namespace std ;
+
+/*creating main() function of the program*/
+
+int main ()
+
+{
+
+/* number definition and assignments.*/
+
+int i = 123456789 ;
+
+short s = 12345 ;
+
+long l = 123456789 ;
+
+float f = 123.56789 ;
+
+double d = 123.56789 ;
+
+/*printing the numbers by various data types of it.*/
+
+cout << "\nValue of int i : " << i << endl ;
+
+cout << "\nValue of short s : " << s << endl ;
+
+cout << "\nValue of long l : " << l << endl ;
+
+cout << "\nValue of float f : " << f << endl ;
+
+cout << "\nValue of double d : " << d << endl ;
+
+}
